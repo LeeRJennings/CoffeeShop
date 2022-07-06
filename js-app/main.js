a4b5208ffeddc474101f160261c1b9ca4a333ca3
LeeRJennings <leerjennings@outlook.com>
2022-07-06 09:00:30 -0500

    new bean varieties can be added

--- js-app/main.js
+++ js-app/main.js
@@ -1,16 +1,15 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
+const stuffEl = document.querySelector("#stuff-goes-here")
+const body = document.querySelector("body")
+
+// ================================== show bean varities ==================================
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     bvList()
-    // getAllBeanVarieties()
-    //     .then(beanVarieties => {
-    //         console.log(beanVarieties);
-    //     })
 });
 
 const bvList = () => {
-    const bvEl = document.querySelector("#bv-info")
     let bvHTMLRep = ""
     getAllBeanVarieties()
     .then(allBv => {
@@ -18,7 +17,7 @@ const bvList = () => {
         for (const bv of allBv) {
             bvHTMLRep += bvHTML(bv)
         }
-        bvEl.innerHTML = `${bvHTMLRep}`
+        stuffEl.innerHTML = `${bvHTMLRep}`
     })
 }
 
@@ -34,4 +33,63 @@ const bvHTML = (bv) => {
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+// ================================== add bean varities ==================================
+const addBVButton = document.querySelector("#addBV-button")
+addBVButton.addEventListener("click", () => {
+    stuffEl.innerHTML = `
+        <div>
+            <label for="name">Name :</label>
+            <input type="text" name="name" id="name">
+        </div>
+
+        <div>
+            <label for="region">Region:</label>
+            <input type="text" name="region" id="region">
+        </div>
+
+        <div>
+            <label for="notes">Notes:</label>
+            <textarea name="notes" id="notes"></textarea>
+        </div>
+
+        <div>
+            <button id="submit-bean-button">Submit Bean!</button>
+        </div>
+    `
+})
+
+// const submitBeanButton = document.querySelector("#submit-bean-button")
+body.addEventListener("click", event => {
+    event.preventDefault()
+    if (event.target.id === "submit-bean-button") {
+        console.log("FUCKIN BUTTON CLICKED")
+        let name = document.querySelector("input[name='name']").value
+        console.log(name)
+        let region = document.querySelector("input[name='region']").value
+        console.log(region)
+        let notes = document.querySelector("textarea[name='notes']").value
+        console.log(notes)
+    
+        const beanObj = {
+            name: name,
+            region: region,
+            notes: notes
+        }
+    
+        addBeanVariety(beanObj)
+        // .then(bvList())
+    }
+})
+
+function addBeanVariety(beanObj) {
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(beanObj) 
+    })
+    .then(response => response.json()) 
 }
